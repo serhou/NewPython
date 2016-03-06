3cfb763fd121c6370353cfc2705045fdd2a9f811
think <serhou@163.com>
2016-03-06 11:07:06 +0800

    更新请求头

--- MyPython/com/bestlove/test/web_crawler_4.py
+++ MyPython/com/bestlove/test/web_crawler_4.py
@@ -1,6 +1,6 @@
 # !/usr/bin/env python3
 # -*- coding:utf-8 -*-
-
+from http.cookiejar import CookieJar
 from urllib import request
 import json
 import socket
@@ -9,19 +9,35 @@ import re
 from datetime import datetime
 import os
 import uuid
+import random
 
 '''
     爬取汽车之家标致308S图片
 '''
 
 proxys = []
-proxys.append({'http': 'http://223.223.182.59:80'})
-proxys.append({'http': 'http://115.159.123.204:80'})
-proxys.append({'http': 'http://182.254.150.120:80'})
-proxys.append({'http': 'http://122.96.59.104:80'})
-user_agent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
+proxys.append({'http': 'http://115.159.100.31:80'})
+proxys.append({'http': 'http://58.176.140.238:80'})
+proxys.append({'http': 'http://119.188.94.145:80'})
+proxys.append({'http': 'http://58.218.198.2:6666'})
+proxys.append({'http': 'http://171.123.115.214:80'})
+proxys.append({'http': 'http://117.83.72.214:80'})
+user_agent = 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0'
+accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+accept_language = 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3'
+accept_encoding = 'gzip, deflate, br'
+connection = 'keep-alive'
 header = {}
 header['User-Agent'] = user_agent
+header['Accept'] = accept
+header['Accept-Language'] = accept_language
+header['Accept-Encoding'] = accept_encoding
+header['Connection'] = connection
+
+#声明一个CookieJar对象实例来保存cookie
+cookie = CookieJar()
+#利用urllib2库的HTTPCookieProcessor对象来创建cookie处理器
+handler=request.HTTPCookieProcessor(cookie)
 
 
 def my_test(dynamic_url):
@@ -29,12 +45,15 @@ def my_test(dynamic_url):
     try:
         url = 'http://car.autohome.com.cn'+dynamic_url
         req = request.Request(url, headers=header)
-        for id in range(0, 1, 1):
-            proxy_support = request.ProxyHandler(proxys[id%4])
-            opener = request.build_opener(proxy_support)
-            request.install_opener(opener)
+        index = random.randint(0, 10)
+        print('使用代理为：',proxys[index%6])
+        proxy_support = request.ProxyHandler(proxys[index%6])
+        opener = request.build_opener(proxy_support, handler)
+        request.install_opener(opener)
+        print('请求地址', url, '中...')
         res = request.urlopen(req).read()
         soup = BeautifulSoup(res, 'html.parser')
+        print('解析网页中...')
         car_all2 = soup.find_all('script')[0]
         str1 = ''
         for s in car_all2:
@@ -62,10 +81,10 @@ def my_test(dynamic_url):
 
 
 def main():
-    # my_test('/photo/20668/1/2922636.html')  # 308S初始化页面
+    my_test('/photo/20668/1/2922636.html')  # 308S初始化页面
     # my_test('/photo/20718/1/2777412.html')  # 3008初始化页面
     # my_test('/photo/20106/1/2829322.html')  # 508初始化页面
-    my_test('/photo/series/21039/1/2858293.html')  # 宝马5系初始化页面
+    # my_test('/photo/series/21039/1/2858293.html')  # 宝马5系初始化页面
 
 
 if __name__ == '__main__':
